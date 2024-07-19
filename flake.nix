148cd7e09eaac714c43c70610e28aef17bf8f67d
ciacon <ciacon>
2024-07-19 11:34:31 +0200

    minor polish

--- flake.nix
+++ flake.nix
@@ -2,17 +2,21 @@
   description = "systemd-nspawn images with NixOS";
 
   inputs = {
-    flake-parts.url = "github:hercules-ci/flake-parts";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+
+    flake-parts.url = "github:hercules-ci/flake-parts";
+
     treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
     treefmt-nix.url = "github:numtide/treefmt-nix";
   };
 
   outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+
     systems = [
       "aarch64-linux"
       "x86_64-linux"
     ];
+
     imports = [
       inputs.treefmt-nix.flakeModule
     ];
